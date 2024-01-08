8decb1639aec7611b97a3e981dc5d1b2011ba8ca
Erik Ernst <eernst@google.com>
2024-01-08 15:08:54 +0100

    Add safe getter to Future

--- lib/safe_future.dart
+++ lib/safe_future.dart
@@ -4,38 +4,38 @@
 
 import 'dart:async';
 
-extension type SafeFuture<T>._(Future<T> _it) implements Future<T> {
-  SafeFuture(FutureOr<T> computation()) : this._(Future(computation));
+extension type SafeFuture<X>._(Future<X> _it) implements Future<X> {
+  SafeFuture(FutureOr<X> computation()) : this._(Future(computation));
 
-  SafeFuture.microtask(FutureOr<T> computation())
+  SafeFuture.microtask(FutureOr<X> computation())
       : this._(Future.microtask(computation));
 
-  SafeFuture.sync(FutureOr<T> computation()) : this._(Future.sync(computation));
+  SafeFuture.sync(FutureOr<X> computation()) : this._(Future.sync(computation));
 
-  SafeFuture.value([FutureOr<T>? value]) : this._(Future.value(value));
+  SafeFuture.value([FutureOr<X>? value]) : this._(Future.value(value));
 
   SafeFuture.error(Object error, [StackTrace? stackTrace])
       : this._(Future.error(error, stackTrace));
 
-  SafeFuture.delayed(Duration duration, [FutureOr<T> computation()?])
+  SafeFuture.delayed(Duration duration, [FutureOr<X> computation()?])
       : this._(Future.delayed(duration, computation));
 
-  static SafeFuture<List<T>> wait<T>(
-    Iterable<Future<T>> futures, {
+  static SafeFuture<List<X>> wait<X>(
+    Iterable<Future<X>> futures, {
     bool eagerError = false,
     void cleanUp(T successValue)?,
   }) =>
-      SafeFuture<List<T>>._(Future.wait<T>(futures,
+      SafeFuture<List<X>>._(Future.wait<X>(futures,
           eagerError: eagerError, cleanUp: cleanUp));
 
-  static SafeFuture<T> any<T>(Iterable<Future<T>> futures) =>
-      SafeFuture<T>._(Future.any<T>(futures));
+  static SafeFuture<X> any<X>(Iterable<Future<X>> futures) =>
+      SafeFuture<X>._(Future.any<X>(futures));
 
-  static SafeFuture<void> forEach<T>(
-    Iterable<T> elements,
+  static SafeFuture<void> forEach<X>(
+    Iterable<X> elements,
     FutureOr action(T element),
   ) =>
-      SafeFuture<void>._(Future.forEach<T>(elements, action));
+      SafeFuture<void>._(Future.forEach<X>(elements, action));
 
   static SafeFuture<void> doWhile(FutureOr<bool> action()) =>
       SafeFuture<void>._(Future.doWhile(action));
@@ -43,14 +43,18 @@ extension type SafeFuture<T>._(Future<T> _it) implements Future<T> {
   SafeFuture<R> then<R>(FutureOr<R> onValue(T value), {Function? onError}) =>
       SafeFuture<R>._(_it.then<R>(onValue, onError: onError));
 
-  SafeFuture<T> catchError(Function onError, {bool test(Object error)?}) =>
-      SafeFuture<T>._(_it.catchError(onError, test: test));
+  SafeFuture<X> catchError(Function onError, {bool test(Object error)?}) =>
+      SafeFuture<X>._(_it.catchError(onError, test: test));
 
-  SafeFuture<T> whenComplete(FutureOr<void> action()) =>
-      SafeFuture<T>._(_it.whenComplete(action));
+  SafeFuture<X> whenComplete(FutureOr<void> action()) =>
+      SafeFuture<X>._(_it.whenComplete(action));
 
-  Stream<T> asStream() => _it.asStream();
+  Stream<X> asStream() => _it.asStream();
 
-  SafeFuture<T> timeout(Duration timeLimit, {FutureOr<T> onTimeout()?}) =>
-      SafeFuture<T>._(_it.timeout(timeLimit, onTimeout: onTimeout));
+  SafeFuture<X> timeout(Duration timeLimit, {FutureOr<X> onTimeout()?}) =>
+      SafeFuture<X>._(_it.timeout(timeLimit, onTimeout: onTimeout));
+}
+
+extension SafeFutureExtension<X> on Future<X> {
+  SafeFuture<X> get safe => SafeFuture<X>._(this);
 }

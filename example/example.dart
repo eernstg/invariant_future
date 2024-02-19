7fa16b9bc747cec2eb34e95d38a46c03e4274df3
Erik Ernst <eernst@google.com>
2024-02-19 19:15:40 +0100

    Example now working

--- example/example.dart
+++ example/example.dart
@@ -23,16 +23,16 @@ void main() async {
  
   // Regular `Future.then` accepts `onError` of type `Function`.
   {
-    final Future<num> fut = Future<int>.error("whatever");
+    final fut = Future<int>.error("This is what the future throws");
     try {
       int i = await fut.then((_) => 1, onError: print);
     } catch (_) {
       print('`onError` has unusable type!');
     }
 
-    final fut2 = Future<int>.error("whatever");
+    final Future<num> fut2 = Future<int>.error("whatever");
     try {
-      await fut.then((_) {}, onError: (_) => 1.5);
+      num n = await fut2.then((n) => n, onError: (_) => "Hello!");
     } catch (_) {
       print('`onError` return value failed run-time type check!');
     }
@@ -40,13 +40,15 @@ void main() async {
 
   // `IFuture.then` accepts `onError` accepts both safe types.
   {
-    final fut = IFuture.error("whatever");
+    final fut = IFuture<num>.error("whatever");
+
     // Compile-time error:
-    //   await fut.then<int>(
-    //     (_) => 1,
-    //     onError: (_) { return 1.5; }.u21,
-    //   );
-    
+    // num n = await fut.then((n) => n, onError: ((_) => "Hello!").u21);
+
+    num n = await fut.then((n) => n, onError: ((_) => 1.5).u21);
+    print('Safe call of `IFuture` returned $n');
   }
+
+
 
 }
